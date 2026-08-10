4f87e8b73d7504eb6569c7030454a8858b29bd87
AbuTalha63 <usmanm6390@gmail.com>
2026-08-10 09:55:04 -0400

    initialized new branch
    this branch will serve to ensure that the frontend, backend and database are all connected

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,17 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
+import { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  try {
+      const res = fetch(`${import.meta.env.VITE_API_URL}/health`);
+      const data = res.json();
+      setStatus(data.ok ? 'API is up ✅' : 'API responded but not ok');
+    } catch (err) {
+      setStatus('Could not reach API ❌');
+    }
+
   return (
     <IonPage>
       <IonHeader>
